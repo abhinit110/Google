fb1935e020f58c06fec2df223edf19433c3d0d3a
Abhinit Singh <abhinit710@gmail.com>
2024-02-15 22:54:16 +0530

    code

---
+++ count_all_path.cpp
@@ -0,0 +1,12 @@
+	int isPossible(vector<vector<int>>paths){
+	    int n=paths.size();
+	    int count =0;
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<n;j++)
+	        count+=paths[i][j];
+	        if(count&1)
+	        return 0;
+	    }
+	    return 1;
+	}
