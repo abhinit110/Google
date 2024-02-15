15ace0d05690a3ded9a213df346508b9862c2099
Abhinit Singh <abhinit710@gmail.com>
2024-02-15 22:56:53 +0530

    Update count_all_path.cpp

--- count_all_path.cpp
+++ count_all_path.cpp
@@ -1,4 +1,6 @@
-//logic used is eulers cycle method
+//lEulerian Cycle: An undirected graph has Eulerian cycle if following two conditions are true. 
+//All vertices with non-zero degree are connected. We don’t care about vertices with zero degree because they don’t belong to Eulerian Cycle or Path (we only consider all edges). 
+//All vertices have even degree.
 int isPossible(vector<vector<int>>paths){
 	    int n=paths.size();
 	    int count =0;
